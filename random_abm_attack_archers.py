a4d84b6eda59f786559e8cddaf9813aec89789cc
Revan-091 <rubenhrex20@gmail.com>
2023-08-01 15:59:29 -0600

    Aug 1

--- random_abm_attack_archers.py
+++ random_abm_attack_archers.py
@@ -46,24 +46,21 @@ class Agent:
 def create_formations(agents):
     soldiers_a = [agent for agent in agents if agent.team == 'A' and agent.agent_type == 'Soldier']
     cavalry_a = [agent for agent in agents if agent.team == 'A' and agent.agent_type == 'Cavalry']
-    archers_a = [agent for agent in agents if agent.team == 'A' and agent.agent_type == 'Archer']
 
     soldiers_b = [agent for agent in agents if agent.team == 'B' and agent.agent_type == 'Soldier']
     cavalry_b = [agent for agent in agents if agent.team == 'B' and agent.agent_type == 'Cavalry']
-    archers_b = [agent for agent in agents if agent.team == 'B' and agent.agent_type == 'Archer']
 
-    formation_a = soldiers_a + cavalry_a + archers_a
-    formation_b = soldiers_b + cavalry_b + archers_b
+    formation_a = soldiers_a + cavalry_a
+    formation_b = soldiers_b + cavalry_b 
 
     return formation_a, formation_b
 
 
 # Function to parse command-line arguments
 def parse_arguments():
-    parser = argparse.ArgumentParser(description='Simulate a battle between two teams with soldiers, archers, and cavalry.')
-    parser.add_argument('--num_soldiers', type=int, default=250, help='Number of soldiers per team')
-    parser.add_argument('--num_archers', type=int, default=125, help='Number of archers per team')
-    parser.add_argument('--num_cavalry', type=int, default=125, help='Number of cavalry per team')
+    parser = argparse.ArgumentParser(description='Simulate a battle between two teams with soldiers, and cavalry.')
+    parser.add_argument('--num_soldiers', type=int, default=600, help='Number of soldiers per team')
+    parser.add_argument('--num_cavalry', type=int, default=400, help='Number of cavalry per team')
     return parser.parse_args()
 
 #Game Theory implementation
@@ -89,45 +86,13 @@ def get_best_response(player, opponent_strategy):
     return strategies[player][best_response_index]
 
 
-# Win-Stay, Lose-Shift strategy implementation
-def win_stay_lose_shift_archer(agent, nearest_enemy):
-    if agent.agent_type == 'Archer':
-        if nearest_enemy.health > 0:
-            # If the enemy is within the Archer's attack range, attack
-            if agent.distance_to(nearest_enemy) <= agent.attack_range:
-                agent.attack(nearest_enemy)
-                if nearest_enemy.health <= 0:
-                    agents.remove(nearest_enemy)
-                    print(f"{nearest_enemy.team} {nearest_enemy.agent_type} died!")
-                    if agent.team == "A":
-                        deaths_B[step + 1] += 1  # Increment deaths counter for Team B
-                        with open('deaths_team_b.csv', 'a') as file:
-                            file.write(f"{step + 1},{deaths_B[step + 1]}\n")
-                    elif agent.team == "B":
-                        deaths_A[step + 1] += 1  # Increment deaths counter for Team A
-                        with open('deaths_team_a.csv', 'a') as file:
-                            file.write(f"{step + 1},{deaths_A[step + 1]}\n")
-            else:
-                # Move away from the nearest enemy agent to maintain formation
-                dx = agent.position[0] - nearest_enemy.position[0]
-                dy = agent.position[1] - nearest_enemy.position[1]
-                dx /= agent.distance_to(nearest_enemy)
-                dy /= agent.distance_to(nearest_enemy)
-                agent.position = (agent.position[0] - dx, agent.position[1] - dy)
-
-    # Store the current action for the next step
-    agent.last_action = "attack" if nearest_enemy.health > 0 else "move"
-
-
-
 def main():
     # Simulation parameters
     args = parse_arguments()
     num_soldiers_a = args.num_soldiers // 2
-    num_archers_a = args.num_archers // 2
     num_cavalry_a = args.num_cavalry // 2
-    num_agents_a = num_soldiers_a + num_archers_a + num_cavalry_a
-    num_agents_b = args.num_soldiers - num_soldiers_a + args.num_archers - num_archers_a + args.num_cavalry - num_cavalry_a
+    num_agents_a = num_soldiers_a  + num_cavalry_a
+    num_agents_b = args.num_soldiers - num_soldiers_a +  args.num_cavalry - num_cavalry_a
     num_agents = num_agents_a + num_agents_b
     max_steps = 1000
 
@@ -140,53 +105,54 @@ def main():
     # Create agents
     agents = []
     num_agents_a = num_agents // 2
-    num_soldiers_a = num_agents_a // 2
-    num_archers_a = num_agents_a // 4  # Assuming 1/4 of Team A agents are Archers
     num_cavalry_a = num_agents_a // 4  # Assuming 1/4 of Team A agents are Cavalry
+    num_soldiers_a = num_agents_a - num_cavalry_a
     num_agents_b = num_agents - num_agents_a
-    num_soldiers_b = num_agents_b // 2
-    num_archers_b = num_agents_b // 4  # Assuming 1/4 of Team B agents are Archers
     num_cavalry_b = num_agents_b // 4  # Assuming 1/4 of Team B agents are Cavalry
+    num_soldiers_b = num_agents_b - num_cavalry_b
 
     # Create agents for team A
-    for _ in range(num_archers_a):
-        position = (random.uniform(0, 53), random.uniform(170, 200))
-        health = 80
-        attack_range = random.uniform(30, 50)  # Archers have a longer attack range
-        attack_strength = random.randint(15, 40)  # Archers hit less hard
-        agents.append(Agent("A", "Archer", position, health, attack_range, attack_strength))
-
     for _ in range(num_soldiers_a):
-        position = (random.uniform(0, 200), random.uniform(140, 170))
+        position1 = (random.uniform(2, 7), random.uniform(105, 160))
+        position2 = (random.uniform(35, 60), random.uniform(165, 170))
+        position3 = (random.uniform(80, 125), random.uniform(155, 165))
+        pos_list = [position1, position2, position3]
+        for i in range(num_soldiers_a):
+            position = random.choice(pos_list)
         health = 100
         attack_range = random.uniform(1, 5)
         attack_strength = random.uniform(20, 30)
         agents.append(Agent("A", "Soldier", position, health, attack_range, attack_strength))
 
     for _ in range(num_cavalry_a):
-        position = (random.uniform(0, 200), random.uniform(120, 140))  # Adjust position as desired
+        position1 = (random.uniform(7, 9), random.uniform(105, 160))
+        position2 = (random.uniform(45, 60), random.uniform(165, 168))
+        pos_list = [position1, position2]
+        for i in range(num_soldiers_a):
+            position = random.choice(pos_list)
         health = 150
         attack_range = random.uniform(6, 10)  # Cavalry has a slightly longer attack range than Soldier
         attack_strength = random.uniform(45, 60)  # Cavalry's attack strength
         agents.append(Agent("A", "Cavalry", position, health, attack_range, attack_strength))
 
     # Create agents for team B
-    for _ in range(num_archers_b):
-        position = (random.uniform(0, 200), random.uniform(0, 30))
-        health = 80
-        attack_range = random.uniform(30, 50)  # Archers have a longer attack range
-        attack_strength = random.randint(15, 40)  # Archers hit less hard
-        agents.append(Agent("B", "Archer", position, health, attack_range, attack_strength))
-
     for _ in range(num_soldiers_b):
-        position = (random.uniform(0, 200), random.uniform(30, 60))
+        position1 = (random.uniform(30, 45), random.uniform(95, 130))
+        position2 = (random.uniform(50, 70), random.uniform(145, 130))
+        pos_list = [position1, position2]
+        for i in range(num_soldiers_a):
+            position = random.choice(pos_list)
         health = 100
-        attack_range = random.uniform(1, 5)  # Archers have a longer attack range
-        attack_strength = random.randint(20, 30)  # Archers hit less hard
+        attack_range = random.uniform(1, 5) 
+        attack_strength = random.randint(20, 30)  
         agents.append(Agent("B", "Soldier", position, health, attack_range, attack_strength))
 
     for _ in range(num_cavalry_b):
-        position = (random.uniform(0, 200), random.uniform(70, 100))  # Adjust position as desired
+        position1 = (random.uniform(25, 35), random.uniform(95, 130))
+        position2 = (random.uniform(50, 70), random.uniform(142, 140))
+        pos_list = [position1, position2]
+        for i in range(num_soldiers_a):
+            position = random.choice(pos_list)
         health = 150
         attack_range = random.uniform(2, 8)  # Cavalry has a slightly longer attack range than Soldier
         attack_strength = random.uniform(45, 60)  # Cavalry's attack strength
@@ -216,11 +182,10 @@ def main():
 
             # Get agents in range of the current agent
             agents_in_range = agent.get_agents_in_range(agents, agent.attack_range)
-            
             if agent.agent_type == 'A':
-                num_friends, num_enemies = agent.count_friends_and_enemies(agents_in_range)
+                #num_friends, num_enemies = agent.count_friends_and_enemies(agents_in_range)
                 if step < 40:
-                    common_point = (100, 150)  # The common point is the center of the map (you can adjust it as desired)
+                    common_point = (0, 150)  # The common point is the center of the map (you can adjust it as desired)
                     agent.retreat_to_common_point(agents, common_point)
                     enemy_formation = formation_b if agent.team == 'A' else formation_a
                     enemy_formation_positions = [a.position for a in enemy_formation]
@@ -238,6 +203,21 @@ def main():
                     agent.retreat_to_common_point(agents, common_point)
                     
 
+                # Attack if an enemy is within the agent's attack range
+                if agent.distance_to(nearest_enemy) <= agent.attack_range:
+                    agent.attack(nearest_enemy)
+                    if nearest_enemy.health <= 0:
+                        agents.remove(nearest_enemy)
+                        print(f"{nearest_enemy.team} {nearest_enemy.agent_type} died!")
+                        if agent.team == "A":
+                            deaths_B[step + 1] += 1  # Increment deaths counter for Team B
+                            with open('deaths_team_b.csv', 'a') as file:
+                                file.write(f"{step + 1},{deaths_B[step + 1]}\n")
+                        elif agent.team == "B":
+                            deaths_A[step + 1] += 1  # Increment deaths counter for Team A
+                            with open('deaths_team_a.csv', 'a') as file:
+                                file.write(f"{step + 1},{deaths_A[step + 1]}\n")
+            
             elif agent.agent_type == 'Soldier' or agent.agent_type == 'Cavalry':
                 # Move towards the nearest enemy agent
                 dx = nearest_enemy.position[0] - agent.position[0]
